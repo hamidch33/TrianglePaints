0c05bb4d9b21ee654bdd76361cb6df85d5239ae7
Hamid <hamid.ch33@gmail.com>
2020-08-25 20:37:42 +0500

    Add project files.

---
+++ TrianglePaints/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace TrianglePaints.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDbConnection _db = new MySqlConnection(ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString);
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TrianglePaints/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrianglePaints.MvcApplication" Language="C#" %>
